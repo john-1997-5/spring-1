5e757b258f752f2f7b7b23011b928369394ff279
Johnson <johnson.arrobo@gmail.com>
2022-11-18 12:15:34 +0100

    Configuración de beans manual con @Configuration & @Bean

--- src/main/java/com/johnson/spring/Application.java
+++ src/main/java/com/johnson/spring/Application.java
@@ -1,13 +1,26 @@
 package com.johnson.spring;
 
+import com.johnson.spring.domain.Vehicle;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        var ctx = SpringApplication.run(Application.class, args);
+        Vehicle vehicle = ctx.getBean("vehicle", Vehicle.class); // con nombre de método
+        Vehicle coolVehicle = ctx.getBean("coolVehicle", Vehicle.class); // con nombre del @Bean
+
+        log.info("normal vehicle: {}", vehicle);
+        log.info("cool vehicle: {}", coolVehicle);
+
+        Vehicle defaultVehicle = ctx.getBean(Vehicle.class); // por default con @Primary
+        log.info("unknown vehicle: {}", defaultVehicle);
+
     }
 
 }
---
+++ src/main/java/com/johnson/spring/domain/Vehicle.java
@@ -0,0 +1,11 @@
+package com.johnson.spring.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Vehicle {
+    private String name;
+}
