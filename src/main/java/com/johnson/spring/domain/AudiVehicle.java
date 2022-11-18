da39db03b44faa94cba2636fe9817bad0fa383ec
Johnson <johnson.arrobo@gmail.com>
2022-11-18 16:02:20 +0100

    dependencias circulares

--- src/main/java/com/johnson/spring/domain/AudiVehicle.java
+++ src/main/java/com/johnson/spring/domain/AudiVehicle.java
@@ -3,6 +3,7 @@ package com.johnson.spring.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component // el bean se llamará audiVehicle (la primer minúscula)
@@ -11,7 +12,12 @@ import org.springframework.stereotype.Component;
 @ToString
 public class AudiVehicle implements Vehicle{
     private String model;
+
+    @Autowired
+    private Person owner;
+
     public AudiVehicle() {
         this.model = "Audi R8";
     }
+
 }
--- src/main/java/com/johnson/spring/domain/Person.java
+++ src/main/java/com/johnson/spring/domain/Person.java
@@ -1,41 +1,18 @@
 package com.johnson.spring.domain;
 
 import lombok.Getter;
+import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
+@ToString
 public class Person {
     private String name = "johnson";
 
     @Autowired
-    //@Qualifier("audiVehicle")
-    private Vehicle audiVehicle; // si a la variable le pongo el mismo nombre que el bean, lo inyecta sin necesidad de un qualifier
+    private Vehicle audiVehicle;
 
-    private Vehicle vehicle2;
-
-    private final Vehicle vehicle3;
-
-    @Autowired
-    public Person(Vehicle teslaVehicle) { // mismo nombre que el bean (he quitado el @Primary o sea que no hay vehículo por defecto)
-        this.vehicle3 = teslaVehicle;
-    }
-
-    @Autowired
-    //@Qualifier("ChevyVehicle")
-    public void setChevyVehicle(Vehicle ChevyVehicle){ // mismo nombre que el bean
-        this.vehicle2 = ChevyVehicle;
-    }
-
-    @Override
-    public String toString() {
-        return "\n Person{" +
-                "\n\t name='" + name + '\'' +
-                "\n\t - attribute injection -> vehicle1: " + audiVehicle +
-                "\n\t - setter injection -> vehicle2: " + vehicle2 +
-                "\n\t - constructor injection -> vehicle3: " + vehicle3 +
-                "\n }";
-    }
 }
