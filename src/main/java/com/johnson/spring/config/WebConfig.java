37fe3920df491d87590be53fedc790439165cd01
Johnson <johnson.arrobo@gmail.com>
2022-11-22 14:32:30 +0100

    definiendo método que recibe montón de @RequestParam (cutre approach)

--- src/main/java/com/johnson/spring/config/WebConfig.java
+++ src/main/java/com/johnson/spring/config/WebConfig.java
@@ -9,6 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("").setViewName("home");
+        registry.addViewController("/contact").setViewName("contact");
         registry.addViewController("/courses").setViewName("courses");
         registry.addViewController("/about").setViewName("about");
         registry.addViewController("/holidays").setViewName("holidays");
--- src/main/java/com/johnson/spring/controller/HomeController.java
+++ src/main/java/com/johnson/spring/controller/HomeController.java
@@ -1,21 +1,25 @@
 package com.johnson.spring.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@Slf4j
 public class HomeController {
 
-    @GetMapping(value = {"/home", "", "/"})
-    public String hello(Model model){
-        model.addAttribute("user", "johnson arrobo");
-        return "home.html";
+    @PostMapping("/saveMsg")
+    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,
+                                    @RequestParam String email, @RequestParam String subject,
+                                    @RequestParam String message) {
+        log.info("saved message!");
+        return new ModelAndView("redirect:/contact");
     }
 
-    @GetMapping("/contact")
-    public String contact() {
-        return "contact.html";
-    }
+
 
 }
