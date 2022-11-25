249d8fedb3112ff387aa5be3646cb26f7ec1e618
Johnson <johnson.arrobo@gmail.com>
2022-11-25 15:42:13 +0100

    ApplicationScope

--- src/main/java/com/johnson/spring/service/ContactService.java
+++ src/main/java/com/johnson/spring/service/ContactService.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
 import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.context.annotation.SessionScope;
 
-@SessionScope
+@ApplicationScope
+//@SessionScope
 //@RequestScope
 @Service
 @Slf4j
