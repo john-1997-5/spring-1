8a1078745f75d0665cb528397e8fdb5969bffacb
Johnson <johnson.arrobo@gmail.com>
2022-11-28 12:26:41 +0100

    configuración de varios usuarios y roles in memory (toy approach)

--- src/main/java/com/johnson/spring/config/ProjectSecurityConfig.java
+++ src/main/java/com/johnson/spring/config/ProjectSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -32,5 +36,23 @@ public class ProjectSecurityConfig {
                 .antMatchers("/about").permitAll())
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("steve")
+                .password("steve")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("johnson")
+                .password("johnson")
+                .roles("USER", "ADMIN")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(user,admin);
     }
 }
