610019e58b6d941b8f30e6d785c15d937aaa86a8
Johnson <johnson.arrobo@gmail.com>
2022-11-29 13:46:02 +0100

    nueva lógica en endpoint para pillar las holidays

--- src/main/java/com/johnson/spring/controller/HolidayController.java
+++ src/main/java/com/johnson/spring/controller/HolidayController.java
@@ -2,7 +2,9 @@ package com.johnson.spring.controller;
 
 import com.johnson.spring.model.Holiday;
 import com.johnson.spring.model.HolidayType;
+import com.johnson.spring.service.HolidayService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,26 +20,20 @@ import java.util.stream.Collectors;
 @Slf4j
 public class HolidayController {
 
+    @Autowired
+    HolidayService holidayService;
+
     @GetMapping("/holidays/{display}")
     public String displayHolidays(@PathVariable String display, Model model) {
         boolean festival = display.equals("all") || display.equals("festival");
         boolean federal = display.equals("all") || display.equals("federal");
 
-        model.addAttribute("festival",festival);
-        model.addAttribute("federal",federal);
+        model.addAttribute("festival", festival);
+        model.addAttribute("federal", federal);
 
-        List<Holiday> holidays = Arrays.asList(
-                new Holiday(" Jan 1 ", "New Year's Day", HolidayType.FESTIVAL),
-                new Holiday(" Oct 31 ", "Halloween", HolidayType.FESTIVAL),
-                new Holiday(" Nov 24 ", "Thanksgiving Day", HolidayType.FESTIVAL),
-                new Holiday(" Dec 25 ", "Christmas", HolidayType.FESTIVAL),
-                new Holiday(" Jan 17 ", "Martin Luther King Jr. Day", HolidayType.FEDERAL),
-                new Holiday(" July 4 ", "Independence Day", HolidayType.FEDERAL),
-                new Holiday(" Sep 5 ", "Labor Day", HolidayType.FEDERAL),
-                new Holiday(" Nov 11 ", "Veterans Day", HolidayType.FEDERAL)
-        );
+        List<Holiday> holidays = holidayService.getHolidays(display.toUpperCase());
 
-        for (HolidayType type: HolidayType.values()) {
+        for (var type : Holiday.Type.values()) {
             model.addAttribute(type.toString(),
                     (holidays.stream()
                             .filter(holiday -> holiday.getType().equals(type))
